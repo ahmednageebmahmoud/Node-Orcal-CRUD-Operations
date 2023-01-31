3acd22e0302561152460e727f0806c80631cf6ce
ahmed <ahmed.nageeb.mahmoud.hssan@gmail.com>
2023-01-31 20:38:10 +0200

    sd

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 //Read Vriabels From .env File To Use Direct From "process.env"
-require('dotenv').config();
-const express = require('express');
-const { join } = require('path');
+require("dotenv").config();
+const express = require("express");
+const { join } = require("path");
 
 //Create Express App
 const app = express();
 
 //Tell Express Where Our View Main Path
-app.set('views', process.cwd() + '/src/views');
+app.set("views", process.cwd() + "/src/views");
 //Tell Express What View Engin We Need To Use
 app.set("view engine", "ejs");
 //To Make Express Detect Static Files Automaticly Without Create Single Router To Each File
-app.use(express.static(join(process.cwd(), '/src/public')));
+app.use(express.static(join(process.cwd(), "/src/public")));
 //For Parsing application/json And Set Linit To Request Body
-app.use(express.json({ limit: '900mb' }));
-app.use(express.urlencoded({ extended: true, limit: '900mb' }));
-
-
+app.use(express.json({ limit: "900mb" }));
+app.use(express.urlencoded({ extended: true, limit: "900mb" }));
 
 /** Load User Module */
-require('./modules/user/router')(app);
+require("./modules/user/router")(app);
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-
-
-
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Express server listening http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.render("index");
 });
+
+//Store DB In Gloable To User It In Ant Where Without Require
+global.db = require("./core/db");
+(async () => {
+  //Migrate Data Base Before Start The App
+  await global.db.migrate();
+
+  //Start App Now
+  let port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Express server listening http://localhost:${port}`);
+  });
+})();
--- core/db.js
+++ core/db.js
@@ -1,22 +1,58 @@
-const oracledb = require('oracledb');
+const oracledb = require("oracledb");
 oracledb.autoCommit = true; //Commita queries
 const dbConfig = {
-    user          : process.env.DB_USERNAME ,  
-    password      : process.env.DB_PASSWORD,  
-    connectString : "IP:1521/NOME_DO_SERVICO",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  connectString: process.env.DB_CONNECTIONSTRING,
 };
 
+module.exports = {
+  getConnection: () => oracledb.getConnection(dbConfig),
 
-module.exports={
-    getConnection:()=>oracledb.getConnection(dbConfig),
+  async migrate() {
     
- async   migrate(){
-    //Open Connection 
-    var connection=this.getConnection();
+    console.log(`
+            ======
+                DataBase Migration Is Started
+            ====`);
+            oracledb.getConnection(dbConfig).then(c=> console.log(c)).catch(err=> console.log(err))
 
-        //Create Users Table If Not Created
-connection.exports()
+            return
+    //Open Connection
+    var connection = await oracledb.getConnection(dbConfig);
 
-        //Create Posts Table If Not Created
-    }
-}
+    //Create Users Table If Not Created
+    connection.execute(`IF  NOT EXISTS (SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
+    
+    BEGIN
+    CREATE TABLE [dbo].[Users](
+       Id bigint primary key identity(1,1),
+       UserName nvarchar(150) Not NULL,
+       Email nvarchar(150) Not NULL,
+       Password nvarchar(150) Not NULL,
+       UserType nvarchar(150) Not NULL
+    ) 
+    END
+    `);
+
+    //Create Posts Table If Not Created
+    connection.execute(`IF  NOT EXISTS (SELECT * FROM sys.objects 
+            WHERE object_id = OBJECT_ID(N'[dbo].[Posts]') AND type in (N'U'))
+            
+            BEGIN
+            CREATE TABLE [dbo].[Posts](
+               Id bigint primary key identity(1,1),
+               Title nvarchar(max) Not NULL,
+               Description nvarchar(max) Not NULL,
+               FkUser_Id bigint Not NULL,
+               FOREIGN KEY (FkUser_Id) REFERENCES Users (Id) Not NULL
+            ) 
+            END`);
+
+    console.log(`
+            ======
+                DataBase Migration Is Finshed
+            ====`);
+  },
+};
